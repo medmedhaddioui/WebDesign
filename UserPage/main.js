aa35be4ed7aa00e9a6073a00d4c876db9ae63e71
Mohammed El Haddioui <mel-hadd@c3r9p1.1337.ma>
2025-09-30 18:04:52 +0100

    end Todolist

--- UserPage/main.js
+++ UserPage/main.js
@@ -3,7 +3,6 @@ const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email')
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
-
 myForm.addEventListener('submit', onSubmit);
 
 function  onSubmit  (e)
